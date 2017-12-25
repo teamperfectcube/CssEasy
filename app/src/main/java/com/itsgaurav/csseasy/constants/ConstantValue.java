0ba1558b5126ee5ef6b5eee7db0cc22fcc6a1baa
aboutgaurav <gaurav.kumar24@outlook.com>
2017-12-25 14:26:08 +0530

    initial commit

--- app/src/main/java/com/itsgaurav/csseasy/constants/ConstantValue.java
+++
@@ -1,8 +0,0 @@
-package com.itsgaurav.csseasy.constants;
-
-public class ConstantValue {
-
-    public static final String CATEGORY_BASICS = "CSS Basics";
-    public static final String CATEGORY_CSS3 = "CSS3";
-    public static final String CATEGORY_RESPONSIVE = "CSS Responsive";
-}
